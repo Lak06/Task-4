1843b11e1851f88ba73b0408131f64e39b85b49f
Sameer <sameersaad505@gmail.com>
2025-06-24 11:44:56 +0100

    uploaded bot1 pkg

---
+++ sim/install/bot1/share/bot1/launch/bot1_gazebo.launch.py
@@ -0,0 +1,29 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import Command, FindExecutable, PathJoinSubstitution
+from launch_ros.substitutions import FindPackageShare
+
+def generate_launch_description():
+    pkg_bot1 = FindPackageShare("bot1")
+    robot_urdf = PathJoinSubstitution([pkg_bot1, "models", "bot_1.urdf"])
+
+    return LaunchDescription([
+        Node(
+            package="robot_state_publisher",
+            executable="robot_state_publisher",
+            parameters=[{"robot_description": Command([FindExecutable(name="xacro"), " ", robot_urdf])}]
+        ),
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource([
+                PathJoinSubstitution([FindPackageShare("gazebo_ros"), "launch", "gazebo.launch.py"])
+            ])
+        ),
+        Node(
+            package="gazebo_ros",
+            executable="spawn_entity.py",
+            arguments=["-entity", "bot1", "-file", robot_urdf],
+            output="screen"
+        )
+    ])
